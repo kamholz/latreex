55b2f2737c0f16becf6504d45f01bc1a4b46a269
David Kamholz <lautgesetz@gmail.com>
2012-12-31 14:40:00 -0500

    fix makepdf.sh

--- makepdf.sh
+++ makepdf.sh
@@ -6,6 +6,6 @@ FILE=$2
 cd $DIR
 latex -interaction batchmode "$FILE.tex" >/dev/null 2>&1
 latex -interaction batchmode "$FILE.tex" >/dev/null 2>&1
-dvips -o "$FILE.eps" "$FILE.dvi" >/dev/null 2>&1
+dvips -E -o "$FILE.eps" "$FILE.dvi" >/dev/null 2>&1
 ps2pdf -dEPSCrop "$FILE.eps" "$FILE.pdf" >/dev/null 2>&1
 rm -f "$FILE.tex" "$FILE.dvi" "$FILE.eps" "$FILE.aux" "$FILE.log" >/dev/null 2>&1

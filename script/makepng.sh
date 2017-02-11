03b420a4b9279d5c6f20fa219d3e0442ddd8d88c
David Kamholz <lautgesetz@gmail.com>
2017-02-11 05:07:08 -0800

    better image preview

--- script/makepng.sh
+++ script/makepng.sh
@@ -8,5 +8,4 @@ xelatex -interaction batchmode "$FILE.tex" >/dev/null 2>&1
 xelatex -interaction batchmode "$FILE.tex" >/dev/null 2>&1
 gs -dSAFER -dBATCH -dNOPAUSE -dDOINTERPOLATE -dTextAlphaBits=4 -dGraphicsAlphaBits=4 \
     -r600 -sDEVICE=pnggray -sOutputFile="$FILE.png" "$FILE.pdf" >/dev/null 2>&1
-mogrify -scale 20% -quality 100 "$FILE.png" >/dev/null 2>&1
 rm -f "$FILE.tex" "$FILE.pdf" "$FILE.aux" "$FILE.log" >/dev/null 2>&1

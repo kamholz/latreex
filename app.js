0b99239ed4b30a3fdade68eb1f1bf9a288778b2c
David Kamholz <lautgesetz@gmail.com>
2017-02-10 23:48:12 -0800

    implement noto font mapping (plus various other cleanup)

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
 var errorHandler = require('errorhandler');
 var stylus = require('stylus');
+
 var ejs = require('ejs');
-var fs = require('fs');
 var execFile = require('child_process').execFile;
+var fs = require('fs');
 var uuid = require('node-uuid');
+
 var config = require('./config');
+var latexTemplate = fs.readFileSync(__dirname + '/lib/latex.ejs', 'utf8');
+var noto = require('./lib/noto');
+var script = require('./lib/script');
+var treeDir = config.treeDir || __dirname + '/trees';
 
 var fontMap = {
     notosans:   'Noto Sans',
@@ -18,10 +23,6 @@ var fontMap = {
     times:      'Times New Roman',
     courier:    'Courier New'
 };
-
-var latexTemplate = fs.readFileSync(__dirname + '/latex.ejs', 'utf8');
-var ucharclasses = fs.readFileSync(__dirname + '/ucharclasses.tex', 'utf8');
-var treeDir = config.treeDir || __dirname + '/trees';
 
 if (!fs.existsSync(treeDir)) fs.mkdirSync(treeDir);
 
@@ -72,23 +73,26 @@ proxyApp.listen(config.port, function () {
 });
 
 var paramDefaults = {
-    linewidth: '0.3pt',
-    treesep: '4ex',
-    levelsep: '2cm',
-    LFTwidth: '15ex',
-    LFTsep: '4pt',
-    orient: 'D',
-    font: 'notosans',
-    style: 'nonflat'
+    linewidth:  '0.3pt',
+    treesep:    '4ex',
+    levelsep:   '2cm',
+    LFTwidth:   '15ex',
+    LFTsep:     '4pt',
+    orient:     'D',
+    font:       'notosans',
+    cjk:        'SC',
+    style:      'nonflat'
 };
 
 var paramValidate = {
-    orient: /^(D|U|R|L)$/,
-    font: /^(notosans|notoserif|notomono|arial|times|courier)$/,
-    style: /^(flat|nonflat)$/
+    orient: /^(?:D|U|R|L)$/,
+    font:   /^(?:notosans|notoserif|notomono|arial|times|courier)$/,
+    cjk:    /^(?:SC|TC|JP|KR)$/,
+    style:  /^(?:flat|nonflat)$/
 };
+
 ['linewidth','treesep','levelsep','LFTwidth','LFTsep'].forEach(function (p) {
-    paramValidate[p] = /^([0-9]+)?\.?[0-9]+(in|mm|cm|pt|em|ex|pc|bp|dd|cc|sp)$/;
+    paramValidate[p] = /^(?:[0-9]+)?\.?[0-9]+(?:in|mm|cm|pt|em|ex|pc|bp|dd|cc|sp)$/;
 });
 
 var orientToRefpoint = {
@@ -110,7 +114,7 @@ function makeLatex(req, res, next) {
         if (p[i] !== undefined && !p[i].match(paramValidate[i])) delete p[i];
     }
     for (var i in paramDefaults) {
-        if (!p[i]) p[i] = paramDefaults[i];
+        if (p[i] === undefined) p[i] = paramDefaults[i];
     }
 
     p.json = JSON.stringify(p, Object.keys(p).sort());
@@ -120,7 +124,6 @@ function makeLatex(req, res, next) {
     p.tree = parseTree(p.tree.split(/\r\n/))[0];
     p.pstTree = function() { return pstNode(p, p.tree, 0).replace(/^\n/,'') };
     p.roman = function(dec) { return roman(dec).toLowerCase() };
-    p.ucharclasses = ucharclasses;
 
     req.file = uuid.v4();
     req.treeName = getTreeName(p.tree.value);
@@ -185,17 +188,18 @@ function pstNode(p, treeNode, depth) {
     var node, afternode;
     var captures = /^(\.?)(.*?)(~?)(\^*)$/.exec(treeNode.value);
 
-    if (captures[1] != '' || captures[2] == '') {
-        node = "\\Tn";
-        afternode = "\\Tp";
+    if (captures[1] !== '' || captures[2] === '') {
+        node = '\\Tn';
+        afternode = '\\Tp';
     } else {
-        node = (captures[3] != '' ? '\\LFTr' : '\\LFTw') + "{"+p.refpoint+"}{"+escapeLatex(captures[2])+"}";
-        afternode = "\\Tp[edge=none]";
+        node = (captures[3] !== '' ? '\\LFTr' : '\\LFTw')
+            + '{'+p.refpoint+'}{'+nodeLabel(captures[2], p)+'}';
+        afternode = '\\Tp[edge=none]';
     }
 
-    str += "\n";
+    str += '\n';
     var i = depth;
-    while (i--) str += "  ";
+    while (i--) str += '  ';
 
     var skip = 0;
     if (p.style === 'flat') {
@@ -204,32 +208,69 @@ function pstNode(p, treeNode, depth) {
         if (skip < 0) skip = 0;
     }
 
-    if (skip > 0) str += "\\skiplevels{"+skip*2+"} ";
+    if (skip > 0) str += '\\skiplevels{'+skip*2+'} ';
 
     if (treeNode.children.length) {
-        str += "\\pstree{"+node+"}{\\pstree{"+afternode+"}{%";
+        str += '\\pstree{'+node+'}{\\pstree{'+afternode+'}{%';
         treeNode.children.forEach(function (x) { str += pstNode(p, x, depth+skip+1) });
-        str += "}}";
+        str += '}}';
     }
     else str += node;
 
-    if (skip > 0) str += " \\endskiplevels";
+    if (skip > 0) str += ' \\endskiplevels';
 
     return str;
 }
 
+function nodeLabel(txt, p) {
+    if (p.font.match(/^Noto/)) {
+        var str = '';
+        var lastFont;
+
+        script.split(txt).forEach(function (chunk) {
+            var substituteFont;
+            if (noto[p.font] && noto[p.font][chunk[1]]) substituteFont = noto[p.font][chunk[1]];
+            else substituteFont = noto.general[chunk[1]];
+
+            var font = substituteFont || p.font;
+
+            if (font !== lastFont) {
+                str += '\\setmainfont{'+font;
+                if (font.match(/CJK$/)) str += ' ' + p.cjk;
+                str += '}';
+
+                lastFont = font;
+            }
+
+            str += escapeLatex(txt);
+        });
+
+        return str;
+    }
+    else return escapeLatex(txt);
+}
+
 function escapeLatex(txt) {
-    return txt.replace(/\\/g,'\\textbackslash{}').replace(/~/g,'\\textasciitilde{}').replace(/[&$%#]/g,'\\$1');
+    return txt
+        .replace(/\\/g,'\\textbackslash{}')
+        .replace(/~/g,'\\textasciitilde{}')
+        .replace(/[&$%#]/g,'\\$1');
 }
 
 function getTreeName(txt) {
-    txt = txt.replace(/~?\^*$/,'').replace(/^\./,'').replace(/[:\\\/]/g,'').trim();
+    txt = txt
+        .replace(/~?\^*$/,'')
+        .replace(/^\./,'')
+        .replace(/[:\\\/]/g,'')
+        .trim();
+
     if (txt === '') txt = 'tree-unnamed';
+
     return txt;
 }
 
-var D = [1,5,10,50,100,500,1000],
-    R = ['I','V','X','L','C','D','M'];
+var D = [1,5,10,50,100,500,1000];
+var R = ['I','V','X','L','C','D','M'];
 
 function roman(dec) {
   var r = '', d = dec;

261752e40d12d00bac316c3cc17b2a0230e5e650
David Kamholz <lautgesetz@gmail.com>
2017-02-11 05:44:36 -0800

    revert debug code from 2 commits ago

--- script/makepdf.sh
+++ script/makepdf.sh
@@ -6,4 +6,4 @@ FILE=$2
 cd $DIR
 xelatex -interaction batchmode "$FILE.tex" >/dev/null 2>&1
 xelatex -interaction batchmode "$FILE.tex" >/dev/null 2>&1
-#rm -f "$FILE.tex" "$FILE.aux" "$FILE.log" missfont.log >/dev/null 2>&1
+rm -f "$FILE.tex" "$FILE.aux" "$FILE.log" missfont.log >/dev/null 2>&1

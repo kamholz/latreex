9b336824bc586f04345b72a9f6fbd568bde361d6
David Kamholz <lautgesetz@gmail.com>
2017-02-12 12:08:52 -0800

    add BabelStone Han support

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ var paramValidate = {
     style:  /^(?:flat|nonflat)$/,
     font:   /^(?:arial|courier|noto_(?:sans|serif|mono)|latex_(?:cmsuper|times_(?:sf|rm|tt))|times)|xcharter$/,
     arabic: /^amiri|arefruqaa|hussaini_nastaleeq|(?:noto_kufi|noto_naskh|noto_nastaliq)$/,
-    cjk:    /^noto_(?:sc|tc|jp|kr)$/,
+    cjk:    /^babelstone|noto_(?:sc|tc|jp|kr)$/,
     greek:  /^(?:alfios|didot|neohellenic|noop|noto_(?:sans|serif)|porson)$/,
     syriac: /^syriac_(?:western|eastern|estrangela)$/,
 };
@@ -57,6 +57,7 @@ var fontMap = {
     amiri:              'Amiri',
     arial:              'Arial',
     arefruqaa:          'Aref Ruqaa',
+    babelstone:         'BabelStone Han',
     courier:            'Courier New',
     didot:              'GFS DidotClassic',
     hussaini_nastaleeq: 'Hussaini Nastaleeq',

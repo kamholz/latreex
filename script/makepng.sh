cda16421d28a630ee43a03b820056b5505d5cfd3
David Kamholz <lautgesetz@gmail.com>
2017-02-14 01:04:00 -0800

    improve preview image generation

--- script/makepng.sh
+++ script/makepng.sh
@@ -6,6 +6,5 @@ FILE=$2
 cd $DIR
 xelatex -interaction batchmode "$FILE.tex" >/dev/null 2>&1
 xelatex -interaction batchmode "$FILE.tex" >/dev/null 2>&1
-gs -dSAFER -dBATCH -dNOPAUSE -dDOINTERPOLATE -dTextAlphaBits=4 -dGraphicsAlphaBits=4 \
-    -r600 -sDEVICE=pnggray -sOutputFile="$FILE.png" "$FILE.pdf" >/dev/null 2>&1
+pdftoppm -r 150 -png -singlefile "$FILE.pdf" "$FILE" >/dev/null 2>&1
 rm -f "$FILE.tex" "$FILE.pdf" "$FILE.aux" "$FILE.log" missfont.log >/dev/null 2>&1

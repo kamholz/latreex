90adc5e3608705a6ee19ab65fead8e56d96eb3ad
David Kamholz <lautgesetz@gmail.com>
2017-02-14 02:06:40 -0800

    change for older netpbm

--- script/makepng.sh
+++ script/makepng.sh
@@ -7,5 +7,5 @@ cd $DIR
 xelatex -interaction batchmode "$FILE.tex" >/dev/null 2>&1
 xelatex -interaction batchmode "$FILE.tex" >/dev/null 2>&1
 pdftoppm -r 600 -singlefile "$FILE.pdf" "$FILE" >/dev/null 2>&1
-pamscale 0.25 -filter=sinc "$FILE.ppm" | pamtopng > "$FILE.png" 2>/dev/null
+pnmscale 0.25 "$FILE.ppm" | pnmtopng > "$FILE.png" 2>/dev/null
 rm -f "$FILE.tex" "$FILE.pdf" "$FILE.aux" "$FILE.log" "$FILE.ppm" missfont.log >/dev/null 2>&1

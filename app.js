4103cb66918f6b913c352ede793ad2357c472e58
David Kamholz <lautgesetz@gmail.com>
2017-02-13 20:50:09 -0800

    add hebrew support

--- app.js
+++ app.js
@@ -28,16 +28,18 @@ var paramDefaults = {
     arabic:     'noto_naskh',
     cjk:        'noto_sc',
     greek:      'noop',
+    hebrew:     'noto_hebrew',
     syriac:     'syriac_estrangela',
 };
 
 var paramValidate = {
     orient: /^(?:D|U|R|L)$/,
     style:  /^(?:flat|nonflat)$/,
-    font:   /^(?:arial|bookman|charis|charter|cm|courier|courier_new|helvetica|noto_(?:sans|serif|mono)|palatino|schoolbook|times|times_mac)$/,
+    font:   /^(?:arial|bookman|cardo|charis|charter|cm|courier|courier_new|helvetica|noto_(?:sans|serif|mono)|palatino|schoolbook|times|times_mac)$/,
     arabic: /^amiri|arefruqaa|hussaini_nastaleeq|(?:noto_kufi|noto_naskh|noto_nastaliq)$/,
     cjk:    /^adobe_kaiti|babelstone|noto_(?:sc|tc|jp|kr)|stkaiti$/,
     greek:  /^(?:alfios|didot|neohellenic|noop|noto_(?:sans|serif)|porson)|times$/,
+    hebrew: /^(?:cardo|david|ezra|mekorot_(?:rashi|vilna)|noto_hebrew)$/,
     syriac: /^syriac_(?:eastern|estrangela|western)$/,
 };
 
@@ -120,6 +122,7 @@ function makeLatex(req, res, next) {
     p.fontspecMap = {
         Arabic: fontMap[p.arabic],
         Emoji:  'Noto Emoji',
+        Hebrew: fontMap[p.hebrew],
         Syriac: fontMap[p.syriac],
     };
     ['Bopomofo','Han','Hangul','Hiragana','Katakana'].forEach(function (i) {

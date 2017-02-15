6d07c18c4792536dabe5c1b392200aeea1391c97
David Kamholz <lautgesetz@gmail.com>
2017-02-15 02:46:55 -0800

    minor fix to makepng.sh

--- script/makepng.sh
+++ script/makepng.sh
@@ -12,5 +12,5 @@ cd $DIR
 xelatex -interaction batchmode "$FILE.tex" >/dev/null 2>&1
 xelatex -interaction batchmode "$FILE.tex" >/dev/null 2>&1
 pdftoppm -r 600 -singlefile "$FILE.pdf" "$FILE" >/dev/null 2>&1
-convert "$FILE.ppm" -scale 20% -quality 100 "$FILE.png" 2>/dev/null
+convert "$FILE.ppm" -scale 20% -quality 100 "$FILE.png" >/dev/null 2>&1
 rm -f "$FILE.tex" "$FILE.pdf" "$FILE.aux" "$FILE.log" "$FILE.ppm" missfont.log >/dev/null 2>&1
